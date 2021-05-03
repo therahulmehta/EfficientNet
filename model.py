6d43da5dd2c13d1424b6acd062d71cc233683b86
therahulmehta <rahul95mehta@gmail.com>
2021-05-03 19:52:22 -0400

    calculated model primitives and architectures

---
+++ model.py
@@ -0,0 +1,28 @@
+#import torch, torch graph blocks, and python math
+import torch 
+import torch.nn as nn
+import math
+
+# foundational model [expandratio, channels, repeats, stride, kernal_size]
+model_primatives = [
+    [1, 16, 1, 1, 3],
+    [6, 24, 2, 2, 3],
+    [6, 40, 2, 2, 5],
+    [6, 80, 3, 2, 3],
+    [6, 112, 3, 1, 5],
+    [6, 192, 4, 2, 5],
+    [6, 320, 1, 1, 3],
+]
+
+# synthetic values for tunning
+# (phi, res, drop rate) 
+phi_values = {
+    "b0": (0, 224, 0.2), 
+    "b1": (0.5, 240, 0.2),
+    "b2": (1, 260, 0.3),
+    "b3": (2, 300, 0.3),
+    "b4": (3, 380, 0.4),
+    "b5": (4, 456, 0.4),
+    "b6": (5, 528, 0.5),
+    "b7": (6, 600, 0.5),
+}
